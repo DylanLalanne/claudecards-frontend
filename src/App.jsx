692e44f75d628ce466f4d333b5e6c783b0ca1e2b
Dylan Lalanne <dylanlalanne7@gmail.com>
2026-05-19 19:04:26 -1000

    add flashcard state and display question.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import {useState} from 'react'
+
+const cards = [
+  {id: 1, question: "What is water made of?", answer:"H20"},
+  {id: 2, question: "What is Air made of?", answer:"Nitrogen, oxygen, carbon dioxide"},
+  {id: 3, question: "What is an apple?", answer:"A delicious fruit"}
+]
 function App() {
-  return <h1>Hello, World!</h1>
+  const [currentIndex, setCurrentIndex] = useState(0)//increment
+  const [showAnswer, setShowAnswer] = useState(false)//bool
+  return <div><h1>{cards[currentIndex].question}</h1></div>
+  
 }
 
 export default App
